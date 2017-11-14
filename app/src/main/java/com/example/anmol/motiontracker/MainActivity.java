879128e097d143fa385ed3682ebd52456eb4cdcb
anjanab96 <anjana.bala96@gmail.com>
2017-11-14 18:58:00 -0500

    UI stuff (separate activities)

--- app/src/main/java/com/example/anmol/motiontracker/MainActivity.java
+++ app/src/main/java/com/example/anmol/motiontracker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.anmol.motiontracker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+        }
 
         Button button = (Button) findViewById(R.id.main);
         button.setOnClickListener(new View.OnClickListener() {
